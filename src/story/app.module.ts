ed973250155807dc491c9a3e44eab393352a0e8d
Verena Gebauer <verena.heinze@study.hs-duesseldorf.de>
2021-01-17 13:36:32 +0100

    create personalization-question component

--- src/story/app.module.ts
+++ src/story/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalizationQuestionComponent } from './personalization-question/personalization-question.component';
 import { ChartComponent } from './chart/chart.component';
 import {FormsModule} from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,6 +11,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
+    PersonalizationQuestionComponent,
     ChartComponent,
     NavbarComponent
   ],
