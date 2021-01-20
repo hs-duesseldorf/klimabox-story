46656a626d8f63d083314efe3a440103d37f65db
EDGARS-LAPPI\Der Edgar <edgar.scheiermann@gmail.com>
2021-01-20 19:07:05 +0100

    WIP: Added Empty Chart Component

---
+++ src/story/chart/chart.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.css']
+})
+export class ChartComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
