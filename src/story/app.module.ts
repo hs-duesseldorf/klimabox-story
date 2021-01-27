c534e7ea89bdf7309b71c327f908ad483a18fe60
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-01-27 17:08:38 +0100

    Import full avatar component

--- src/story/app.module.ts
+++ src/story/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FullAvatarComponent } from "./full-avatar/full-avatar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FullAvatarComponent
   ],
   imports: [
     BrowserModule,
