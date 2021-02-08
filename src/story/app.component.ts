01950ade31e1b019bda5b9c8d233187338954a93
Verena Gebauer <verena.heinze@study.hs-duesseldorf.de>
2021-02-08 14:53:17 +0100

    delete code from app.component

--- src/story/app.component.ts
+++ src/story/app.component.ts
@@ -9,15 +9,7 @@ import {PersonalQuestion} from './personalization-question/personalQuestion';
 // @ts-ignore
 export class AppComponent {
   title = 'Klimabox-Story';
-  question: PersonalQuestion[] = [];
 
   ngOnInit(): void {
-    let personalQuestionFood = PersonalQuestion.createWith('Was isst du gerne?', 'assets/images/personalization-question/kapitel1_table_background.png' );
-    personalQuestionFood.addChoice('assets/images/personalization-question/fleisch.png', '#', '-5', '6');
-    personalQuestionFood.addChoice('assets/images/personalization-question/kaese.png', '#', '25', '5');
-    personalQuestionFood.addChoice('assets/images/personalization-question/avocado.png', '#', '5', '28');
-    personalQuestionFood.addChoice('assets/images/personalization-question/eier.png', '#', '25', '36');
-
-    this.question.push(personalQuestionFood);
   }
 }
