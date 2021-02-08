a57a79d9a786ad3861a331defd18690a0fad32cc
Mesorili <felix.rademacher@gtb.com>
2021-02-08 13:21:17 +0100

    Fertigstellung Intro Komponente Fehler mit trigger

--- src/story/app.module.ts
+++ src/story/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
