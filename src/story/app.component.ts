15faa942db1a4f6a1039ca541f65bce00293a6c2
Verena Gebauer <verena.heinze@study.hs-duesseldorf.de>
2021-02-08 23:33:58 +0100

    delete app.component code

--- src/story/app.component.ts
+++ src/story/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {MassVisualization} from './mass-visualization/massVisualization';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +8,7 @@ import {MassVisualization} from './mass-visualization/massVisualization';
 // @ts-ignore
 export class AppComponent {
   title = 'Klimabox-Story';
-  massVisualization: MassVisualization[] = [];
 
   ngOnInit(): void {
-    let text = 'isst ein Mensch \n in seinem Leben'
-    let massVisualizationPig = MassVisualization.createWith(
-      'assets/images/mass-visualization/massvisualize_pig.png',
-      'rgba(105, 64, 37, 1), rgba(170, 97, 139, 1)',
-      '46 Schweine',
-      text,
-    );
-    this.massVisualization.push(massVisualizationPig);
   }
 }
