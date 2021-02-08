0ed7ab59b9f5dff7295e641cef8e862e569463e2
Verena Gebauer <verena.heinze@study.hs-duesseldorf.de>
2021-02-08 22:19:50 +0100

    create mass-visualization component

--- src/story/app.component.ts
+++ src/story/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-import {PersonalQuestion} from './personalization-question/personalQuestion';
+import {Component} from '@angular/core';
+import {MassVisualization} from './mass-visualization/massVisualization';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 // @ts-ignore
 export class AppComponent {
   title = 'Klimabox-Story';
+  massVisualization: MassVisualization[] = [];
 
   ngOnInit(): void {
+    let text = 'isst ein Mensch \n in seinem Leben'
+    let massVisualizationPig = MassVisualization.createWith(
+      'assets/images/mass-visualization/massvisualize_pig.png',
+      'rgba(105, 64, 37, 1), rgba(170, 97, 139, 1)',
+      '46 Schweine',
+      text,
+    );
+    this.massVisualization.push(massVisualizationPig);
   }
 }
