f09914574ffce57f04d6065d1f03e2dad71e4e0d
Verena Gebauer <verena.heinze@study.hs-duesseldorf.de>
2021-03-17 09:26:16 +0100

    implement disableScroll method

--- src/story/personalization-question/personalization-question.component.ts
+++ src/story/personalization-question/personalization-question.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import {PersonalQuestion} from './personalQuestion';
 
 @Component({
   selector: 'app-personalization-question',
   templateUrl: './personalization-question.component.html',
-  styleUrls: ['./personalization-question.component.css']
+  styleUrls: ['./personalization-question.component.css'],
 })
 // @ts-ignore
 export class PersonalizationQuestionComponent implements OnInit {
@@ -17,12 +18,25 @@ export class PersonalizationQuestionComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (this.personalQuestions.length  > 0) {
+    if (this.personalQuestions.length > 0) {
       this.show(this.personalQuestions[0]);
     }
   }
 
   show(question: PersonalQuestion): void {
     this.questionToShow = question;
+  }
+
+  disableScrolling() {
+    let x = window.scrollX;
+    let y = window.scrollY;
+    window.onscroll = function () {
+      window.scrollTo(x, y);
+    };
+  }
+
+  enableScrolling() {
+    window.onscroll = function () {
+    };
   }
 }
