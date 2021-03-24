b0f1d1f291da62ae0cf3fa8d968db2b6e26ee6ae
alissa <alissa.avt@gmail.com>
2021-03-24 19:31:55 +0100

    wip: bike chapter

--- src/story/content-bike-chapter-two/content-bike-chapter-two.component.ts
+++ src/story/content-bike-chapter-two/content-bike-chapter-two.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import {gsap} from 'gsap';
 
 
 @Component({
@@ -12,6 +14,20 @@ export class ContentBikeChapterTwoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+    this.scroll();
+  }
+
+  scroll(): void {
+    gsap.to('#bikeAccordion', {
+      scrollTrigger: {
+        trigger: '#triggerDiv',
+        start: '+=0px',
+        scrub: true,
+        end: '+=200px'
+      },
+      top: '-200%'
+    });
   }
 
 }
