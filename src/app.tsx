b12b38acc3b29e3dbe36a109d570182ea4b76388
Lem-Joe <lemjoe@hotmail.de>
2021-05-08 14:44:07 +0200

    #58 added State for background

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, {useState} from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { HelmetProvider } from "react-helmet-async";
 
@@ -8,21 +8,32 @@ import { Home } from "./pages/home";
 import { Stories } from "./pages/stories";
 import { Blog } from "./pages/blog";
 
+
 export const App: React.FC = () => {
   const queryClient = React.useMemo(() => new QueryClient(), []);
 
+  const [background, setBackground] = useState({
+    backgroundImage: "",
+    height: "120%",
+    Zindex: "-1",
+    backgroundPosition: "100% 80%",
+    backgroundClip: "padding-box",
+  });
+
   return (
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Switch>
-            <Route path="/blog" component={Blog} />
-            <Route path="/story" component={Stories} />
-            <Route exact path="/" component={Home} />
-            <Route component={NotFound} />
-          </Switch>
-        </Router>
-      </QueryClientProvider>
-    </HelmetProvider>
+    <div className="absolute top-0 left-0 w-full bg-no-repeat bg-cover bg-fixed p-24" style = {background}>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <Switch>
+              <Route path="/blog" render={()=> <Blog backgroundFunction={setBackground} />} />
+              <Route path="/story" render={()=> <Stories backgroundFunction={setBackground} />} />
+              <Route exact path="/" render={()=> <Home backgroundFunction={setBackground} />} />
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </QueryClientProvider>
+      </HelmetProvider>
+    </div>
   );
 };
