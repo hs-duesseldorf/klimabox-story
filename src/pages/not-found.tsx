222169816974bf442edce126ae88c956c5bc47bd
Paul Kretschel <paul.kretschel@icloud.com>
2021-05-11 18:34:15 +0200

    Change 404 page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -5,7 +5,8 @@ import { SEO } from "../components/seo";
 export const NotFound: React.FC = () => (
   <Layout>
     <SEO title="404" />
-    <div className="center-box mt-24 text-4xl">
+    <div className="center-box mt-40">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
       <p>Die Seite konnte nicht gefunden werden.</p>
     </div>
   </Layout>
