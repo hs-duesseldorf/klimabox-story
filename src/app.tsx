6272c378d56fdac6efa78140adf26416bb04a5c8
Paul Kretschel <paul.kretschel@icloud.com>
2021-05-11 15:38:50 +0200

    Remove typescript warnings

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { HelmetProvider } from "react-helmet-async";
 
