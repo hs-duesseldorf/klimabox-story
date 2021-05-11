0f4b6cac42813dd739797d2dbc3f561ba414e9b7
Paul Kretschel <paul.kretschel@icloud.com>
2021-05-11 18:24:07 +0200

    Remove unused component

--- src/components/page-header.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const PageHeader: React.FC = ({ children }) => (
-  <div className="center-box">
-    <h1 className="text-4xl mb-4 font-bold">{children}</h1>
-  </div>
-);
