5257184e34650fa3d8c006d485ee0463802fcf57
Lem-Joe <lemjoe@hotmail.de>
2021-05-22 20:26:32 +0200

    #66 created new Component for buildings

--- src/components/chapter2/start/buildings/buildingA.tsx
+++ src/components/chapter2/start/buildings/buildingA.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import styles from "./start.module.css";
-import img from "./asset/kapitel1_intro_building-1.png";
+import styles from "../start.module.css";
+import img from "../asset/kapitel1_intro_building-1.png";
 
 export const BuildingA: React.FC = () => (
     <img
--- src/components/chapter2/start/buildings/buildingC.tsx
+++ src/components/chapter2/start/buildings/buildingC.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import styles from "./start.module.css";
-import img from "./asset/kapitel1_intro_building-3.png";
+import styles from "../start.module.css";
+import img from "../asset/kapitel1_intro_building-3.png";
 
 export const BuildingC: React.FC = () => (
     <img
