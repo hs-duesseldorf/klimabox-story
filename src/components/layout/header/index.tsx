c850e1eb63ce33a1bd4b65b8d7a8714aa698ef94
Paul Kretschel <paul.kretschel@icloud.com>
2021-06-07 18:47:15 +0200

    Move desktop nav to separate directory

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,70 +1,44 @@
 import React from "react";
 import cn from "classnames";
-import { Link, useLocation } from "react-router-dom";
-import { Button } from "./button";
-import { SearchButton } from "./search-button";
+import { Link } from "react-router-dom";
+import { DesktopNav } from "./desktop-nav";
 
-export const Header: React.FC<{ onDark?: boolean }> = ({ onDark }) => {
-  const { pathname } = useLocation();
+export type NavItem = {
+  path: string;
+  label: string;
+};
 
-  return (
-    <div className="absolute w-full top-0 pt-6 pb-6 mb-3 z-10">
-      <div className="center-box">
-        <div
-          className={cn(
-            "uppercase tracking-wider flex items-center justify-between",
-            onDark && "text-white"
-          )}
-        >
-          <div className="flex items-center">
-            <div
-              className={cn(
-                "text-3xl pb-1 mr-6",
-                onDark ? "text-em1-light" : "text-em1"
-              )}
-            >
-              <Link
-                to="/"
-                className="inline-block pr-10 relative font-semibold"
-              >
-                <div
-                  className={cn(
-                    "absolute top-1 bottom-0 right-0 w-1",
-                    onDark ? "bg-em1-light" : "bg-em1"
-                  )}
-                />
-                Klimabox
-              </Link>
-            </div>
-            <div className="pb-1.5 text-xl sm:text-2xl text-right sm:flex items-end sm:-mx-3">
-              {[
-                ["Story", "/"],
-                ["Archiv", "/blog"],
-              ].map(([label, path], i) => (
-                <div key={i} className="sm:mx-3">
-                  <Link
-                    to={path}
-                    className={cn(
-                      "inline-block pt-1 border-b-2 border-transparent transition",
-                      onDark ? "hover:border-white" : "hover:border-black",
-                      pathname === path && "font-extrabold"
-                    )}
-                  >
-                    {label}
-                  </Link>
-                </div>
-              ))}
-            </div>
+const items: NavItem[] = [
+  { path: "/", label: "Story" },
+  { path: "/blog", label: "Archiv" },
+];
+
+export const Header: React.FC<{ onDark?: boolean }> = ({ onDark }) => (
+  <div className="absolute w-full top-0 pt-6 pb-6 mb-3 z-10">
+    <div className="center-box">
+      <div className={cn("uppercase tracking-wider", onDark && "text-white")}>
+        <div className="flex items-center">
+          <div
+            className={cn(
+              "text-3xl pb-1 mr-6",
+              onDark ? "text-em1-light" : "text-em1"
+            )}
+          >
+            <Link to="/" className="inline-block pr-10 relative font-semibold">
+              <div
+                className={cn(
+                  "absolute top-1 bottom-0 right-0 w-1",
+                  onDark ? "bg-em1-light" : "bg-em1"
+                )}
+              />
+              Klimabox
+            </Link>
           </div>
-          <div className="flex items-center">
-            <SearchButton />
-            <Button onDark={onDark}>Einloggen</Button>
-            <Button buttonOutline={true} onDark={onDark}>
-              Registrieren
-            </Button>
+          <div className="flex-1">
+            <DesktopNav items={items} onDark={onDark} />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
