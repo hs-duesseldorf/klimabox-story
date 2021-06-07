4dffa19f6c037407862184a6ea1672ade76cc2a0
Paul Kretschel <paul.kretschel@icloud.com>
2021-06-07 17:52:22 +0200

    Refactor search button

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import cn from "classnames";
 import { Link, useLocation } from "react-router-dom";
 import { Button } from "./button";
+import { SearchButton } from "./search-button";
 
 export const Header: React.FC<{ onDark?: boolean }> = ({ onDark }) => {
   const { pathname } = useLocation();
@@ -56,19 +57,7 @@ export const Header: React.FC<{ onDark?: boolean }> = ({ onDark }) => {
             </div>
           </div>
           <div className="flex items-center">
-            <svg
-              width="31.479"
-              height="31"
-              viewBox="0 0 31.479 31"
-              className="w-16"
-              fill="currentColor"
-            >
-              <path
-                id="Icon_map-search"
-                d="M24.474,20a12.022,12.022,0,0,0,1.8-6.325A12.323,12.323,0,0,0,13.858,1.44,12.327,12.327,0,0,0,1.44,13.673,12.323,12.323,0,0,0,13.862,25.9a12.483,12.483,0,0,0,6.422-1.774L28.73,32.44l4.189-4.127ZM13.86,21.24a7.635,7.635,0,0,1-7.683-7.559,7.684,7.684,0,0,1,15.366,0A7.641,7.641,0,0,1,13.86,21.24Z"
-                transform="translate(-1.44 -1.44)"
-              />
-            </svg>
+            <SearchButton />
             <Button onDark={onDark}>Einloggen</Button>
             <Button buttonOutline={true} onDark={onDark}>
               Registrieren
