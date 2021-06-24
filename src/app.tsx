8dbc44fa45256c51702870659f0b5034271e2802
Paul Kretschel <paul.kretschel@icloud.com>
2021-06-24 11:36:34 +0200

    Remove dependency "react-query"

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { HelmetProvider } from "react-helmet-async";
 
 import { WPGraphQLClient } from "./components/blog/wp/graphql-client";
@@ -15,27 +14,21 @@ import { Post } from "./pages/post";
 import { Chapter1 } from "./pages/chapter1";
 import { Chapter2 } from "./pages/chapter2";
 
-export const App: React.FC = () => {
-  const queryClient = React.useMemo(() => new QueryClient(), []);
+export const App: React.FC = () => (
+  <HelmetProvider>
+    <WPGraphQLClient>
+      <Router>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/vom-esstisch-in-die-arktis" component={Chapter1} />
+          <Route path="/mobilitaet" component={Chapter2} />
 
-  return (
-    <HelmetProvider>
-      <WPGraphQLClient>
-        <QueryClientProvider client={queryClient}>
-          <Router>
-            <ScrollToTop />
-            <Switch>
-              <Route path="/vom-esstisch-in-die-arktis" component={Chapter1} />
-              <Route path="/mobilitaet" component={Chapter2} />
-
-              <Route path="/blog/:slug" component={Post} />
-              <Route path="/blog" component={Blog} />
-              <Route exact path="/" component={Stories} />
-              <Route component={NotFound} />
-            </Switch>
-          </Router>
-        </QueryClientProvider>
-      </WPGraphQLClient>
-    </HelmetProvider>
-  );
-};
+          <Route path="/blog/:slug" component={Post} />
+          <Route path="/blog" component={Blog} />
+          <Route exact path="/" component={Stories} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </WPGraphQLClient>
+  </HelmetProvider>
+);
--- src/components/blog/wp/config.ts
+++
@@ -1,1 +0,0 @@
-export const WP_REST_URL = "https://klimabox.mirevi.de/wp-json/wp/v2";
