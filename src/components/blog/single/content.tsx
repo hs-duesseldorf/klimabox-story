3f314aa8062272dbb55bb92a2cc80f40a372762a
Paul Kretschel <paul.kretschel@icloud.com>
2021-06-24 12:24:30 +0200

    Fix usage of tailwind typography plugin

--- src/components/blog/single/content.tsx
+++ src/components/blog/single/content.tsx
@@ -4,7 +4,7 @@ import { PostData } from "../wp";
 
 export const PostContent: React.FC<PostData> = ({ post }) => (
   <div
-    className="prose-lg"
+    className="prose prose-lg"
     dangerouslySetInnerHTML={{ __html: post.content }}
   />
 );
