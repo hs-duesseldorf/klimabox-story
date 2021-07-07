2881d13edc1f60ba928796aebd0151d8ddf9f8c9
Paul Kretschel <paul.kretschel@icloud.com>
2021-07-07 12:07:57 +0200

    Only show feed skeleton on first render

--- src/components/blog/feed/index.tsx
+++ src/components/blog/feed/index.tsx
@@ -4,8 +4,13 @@ import { useFeed, FeedItemData } from "../wp";
 import { Post } from "./post";
 import { FilterControl } from "./filter-control";
 
-export const Feed: React.FC = () => {
-  const { error, data } = useFeed();
+function PostList() {
+  const { loading, error, data } = useFeed();
+  const [firstRenderDone, setFirstRenderDone] = React.useState(false);
+
+  React.useEffect(() => {
+    if (data) setFirstRenderDone(true);
+  }, [data]);
 
   const items = React.useMemo<Array<FeedItemData | undefined>>(
     () =>
@@ -17,14 +22,20 @@ export const Feed: React.FC = () => {
 
   if (error) return <div>Es ist ein Fehler aufgetreten.</div>;
 
+  if (loading && firstRenderDone) return <div>Loading...</div>;
+
   return (
-    <>
-      <FilterControl />
-      <div className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:gap-x-10 xl:gap-16 2xl:grid-cols-3">
-        {items.map((data, i) => (
-          <Post data={data} key={i} />
-        ))}
-      </div>
-    </>
+    <div className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:gap-x-10 xl:gap-16 2xl:grid-cols-3">
+      {items.map((data, i) => (
+        <Post data={data} key={i} />
+      ))}
+    </div>
   );
-};
+}
+
+export const Feed: React.FC = () => (
+  <>
+    <FilterControl />
+    <PostList />
+  </>
+);
