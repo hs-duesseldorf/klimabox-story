1272d45d311738a2005c29c02e7f1e0dfa443a89
Paul Kretschel <paul.kretschel@icloud.com>
2021-07-07 23:17:49 +0200

    Change style of loading spinner

--- src/components/blog/feed/spinner/index.tsx
+++ src/components/blog/feed/spinner/index.tsx
@@ -1,6 +1,11 @@
-import React from "react";
 import styles from "./spinner.module.css";
 
 export function Spinner() {
-  return <div className={`${styles.base} mx-auto pointer-events-none`} />;
+  return (
+    <div className={styles.base}>
+      <div />
+      <div />
+      <div />
+    </div>
+  );
 }
