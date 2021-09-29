57d9ec43003bee99b2b87bd3a377297f2a0a6033
Philipp Doll <philipp.doll@9elments.com>
2021-09-29 08:25:08 +0200

    add category select for logged in user

--- src/components/blog/feed/filter-control/categories.tsx
+++ src/components/blog/feed/filter-control/categories.tsx
@@ -1,4 +1,7 @@
 import cn from "classnames";
+import React, { useEffect, useState } from "react";
+import { getTopics } from "../../../../users_api/UserAPI";
+import TopicInterface from "../../../user-dashboard";
 import { useCategories } from "../../wp/categories";
 import { useFilteredCategories } from "../fitering";
 
@@ -23,7 +26,53 @@ const Button: React.FC<{ onClick(): void; active: boolean }> = ({
 };
 
 export function CategoriesFilterControl() {
+  const [user, setUser] = useState(undefined);
+  const [userTopicsSelected, setUserTopicsSelected] = useState<boolean>(false);
+  const [topics, setTopics] = useState<TopicInterface[]>([]);
+
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('user');
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setUser(foundUser);
+    } else {
+      setUser(undefined);
+    }
+    fetchTopics();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [localStorage.getItem('user')]);
+
   const { loading, error, data } = useCategories();
+
+  const selectUserAll = () => {
+    if (data) {
+      topics.forEach((topic) => {
+        const foundCategory = data.categories.nodes.find((category) => category.name === topic.name);
+        if (foundCategory) {
+          select(foundCategory);
+        }
+      })
+    }
+  }
+
+  const fetchTopics = async () => {
+    let response;
+    try {
+      response = await getTopics()
+    } catch(error) {
+      setTopics([])
+    }
+    let responseTopics: Array<TopicInterface> = []
+    if (response) {
+      // @ts-ignore
+      response.forEach(element => {
+        responseTopics.push(element)
+      });
+      setTopics(responseTopics)
+    }
+  }
+
   const {
     items: selectedCategories,
     add: select,
@@ -38,6 +87,19 @@ export function CategoriesFilterControl() {
       <Button active={!selectedCategories.length} onClick={unselectAll}>
         Alle Kategorien
       </Button>
+      { user && (
+        <Button active={userTopicsSelected} onClick={() => {
+          if (!userTopicsSelected) {
+            selectUserAll();
+            setUserTopicsSelected(true);
+          } else {
+            unselectAll();
+            setUserTopicsSelected(false);
+          }
+        }}>
+          Meine Kategorien
+        </Button>
+      )}
       {data.categories.nodes.map((category, i) => {
         const isSelected = selectedCategories.indexOf(category) >= 0;
 
