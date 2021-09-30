9edf2eda1f5fbab163829dbda0ab423db028596f
alot210 <alinaotten@web.de>
2021-09-30 13:44:11 +0200

    fix-side nav in chapter 1 and add link to chapter 3; add cursor-pointer on clickable elements

--- src/components/chapter3/start/scene/sceneComponents/car.tsx
+++ src/components/chapter3/start/scene/sceneComponents/car.tsx
@@ -6,7 +6,7 @@ import Plx from "react-plx";
 
 export const Car: React.FC<{ parallaxData: []; }> = ({ parallaxData, }) => {
   return (
-      <Plx className={`absolute ${styles.car}`} parallaxData={parallaxData} animateWhenNotInViewport={true}>
+      <Plx className={`absolute ${styles.car} cursor-pointer`} parallaxData={parallaxData} animateWhenNotInViewport={true}>
         <img id="car" src={img} alt="car" className={` w-full`}/>
       </Plx>
   );
